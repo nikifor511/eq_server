17e011d1e5e7e45cd6b299cba18fe3885ed8ffac
nikifor511 <deadnikifor@gmail.com>
2020-08-03 22:17:11 +0300

    server.py

--- server.py
+++ server.py
@@ -1,52 +1,114 @@
-import socket, select
+# Python program to implement server side of chat room. 
+import socket 
+import select 
+import sys 
+import _thread
 
-CONNECTION_LIST = []  # list of socket clients
-RECV_BUFFER = 4096  # Advisable to keep it as an exponent of 2
-PORT = 58583
+"""The first argument AF_INET is the address domain of the 
+socket. This is used when we have an Internet Domain with 
+any two hosts The second argument is the type of socket. 
+SOCK_STREAM means that data or characters are read in 
+a continuous flow."""
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
+server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1) 
+
+# checks whether sufficient arguments have been provided 
+# if len(sys.argv) != 3:
+# 	print("Correct usage: script, IP address, port number")
+# 	exit()
+
+# takes the first argument from command prompt as IP address 
+IP_address = ""
+
+# takes second argument from command prompt as port number 
+Port = 58583
+
+""" 
+binds the server to an entered IP address and at the 
+specified port number. 
+The client must be aware of these parameters 
+"""
+server.bind((IP_address, Port)) 
+
+""" 
+listens for 100 active connections. This number can be 
+increased as per convenience. 
+"""
+server.listen(100) 
+
+list_of_clients = []
+
+
+def clientthread(conn, addr): 
+
+	# sends a message to the client whose user object is conn 
+	conn.send("Welcome to this chatroom!".encode())
+
+	while True: 
+			try: 
+				message = conn.recv(2048) 
+				if message: 
+
+					"""prints the message and address of the 
+					user who just sent the message on the server 
+					terminal"""
+					print("<" + addr[0] + "> " + message)
+
+					# Calls broadcast function to send message to all 
+					message_to_send = "<" + addr[0] + "> " + message 
+					broadcast(message_to_send, conn) 
+
+				else: 
+					"""message may have no content if the connection 
+					is broken, in this case we remove the connection"""
+					remove(conn) 
+
+			except: 
+				continue
+
+"""Using the below function, we broadcast the message to all 
+clients who's object is not the same as the one sending 
+the message """
+def broadcast(message, connection): 
+	for clients in list_of_clients: 
+		if clients!=connection: 
+			try: 
+				clients.send(message) 
+			except: 
+				clients.close() 
+
+				# if the link is broken, we remove the client 
+				remove(clients) 
+
+"""The following function simply removes the object 
+from the list that was created at the beginning of 
+the program"""
+def remove(connection): 
+	if connection in list_of_clients: 
+	    list_of_clients.remove(connection)
 
 
 def start():
-    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    # this has no effect, why ?
-    server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    server_socket.bind(("", PORT))
-    server_socket.listen(10)
+    while True:
 
-    # Add server socket to the list of readable connections
-    CONNECTION_LIST.append(server_socket)
+        """Accepts a connection request and stores two parameters, 
+        conn which is a socket object for that user, and addr 
+        which contains the IP address of the client that just 
+        connected"""
+        conn, addr = server.accept()
 
-    print("server started on port " + str(PORT))
+        """Maintains a list of clients for ease of broadcasting 
+        a message to all available people in the chatroom"""
+        list_of_clients.append(conn)
 
-    while 1:
-        # Get the list sockets which are ready to be read through select
-        read_sockets, write_sockets, error_sockets = select.select(CONNECTION_LIST, [], [])
+        # prints the address of the user that just connected
+        print(addr[0] + " connected")
 
-        for sock in read_sockets:
+        # creates and individual thread for every user
+        # that connects
+        _thread.start_new_thread(clientthread,(conn,addr))
+        # my_thread = threading.Thread(target=clientthread, args=(i,))
+        # my_thread.start()
 
-            # New connection
-            if sock == server_socket:
-                # Handle the case in which there is a new connection recieved through server_socket
-                sockfd, addr = server_socket.accept()
-                CONNECTION_LIST.append(sockfd)
-                print("Client (%s, %s) connected" % addr)
-
-            # Some incoming message from a client
-            else:
-                # Data recieved from client, process it
-                try:
-                    # In Windows, sometimes when a TCP program closes abruptly,
-                    # a "Connection reset by peer" exception will be thrown
-                    data = sock.recv(RECV_BUFFER)
-                    # echo back the client message
-                    if data:
-                        sock.send('OK ... ' + data)
-
-                # client disconnected, so remove from socket list
-                except:
-                    # broadcast_data(sock, "Client (%s, %s) is offline" % addr)
-                    print("Client (%s, %s) is offline" % addr)
-                    sock.close()
-                    CONNECTION_LIST.remove(sock)
-                    continue
-
-    server_socket.close()
+    conn.close()
+    server.close()
